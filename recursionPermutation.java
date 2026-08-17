f9e94fd31f27ed40dcad34c47657cd89f71e7a6e
Pranay Fanidhare <pranayfanidhare2412@gmail.com>
2026-08-17 21:01:45 +0530

    Fixed the error

--- recursionPermutation.java
+++ recursionPermutation.java
@@ -1,13 +1,13 @@
 public class recursionPermutation {
     public static void printPermutation(String str, int idx, String perm) {
-        if(str.length() == ){
+        if(str.length() == 0 ){
             System.out.println(perm);
             return;
         }
 
         for(int i=0; i<str.length(); i++){
             char currChar = str.charAt(i);
-            String newStr = str.substring(0, 1) + str.substring(i+1);
+            String newStr = str.substring(0, i) + str.substring(i+1);
             printPermutation(newStr, idx+1, perm+currChar);
         }
     }
