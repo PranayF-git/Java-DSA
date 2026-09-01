33fc279d0ab97f2dfc118276618332165754db0f
Pranay Fanidhare <pranayfanidhare2412@gmail.com>
2026-09-01 20:21:31 +0530

    New recursion problem

---
+++ recursionTiling.java
@@ -0,0 +1,16 @@
+public class recursionTiling {
+    public static int placeTiles(int n, int m) {
+        if (n < m) {
+            return 1;
+        } else if (n == m) {
+            return 2;
+        }
+
+        return placeTiles(n - 1, m) + placeTiles(n - m, m);
+    }
+
+    public static void main(String args[]) {
+        int n = 5, m = 3;
+        System.out.println(placeTiles(n, m));
+    }
+}
